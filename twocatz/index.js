792b1478ad49f1a3e61977efefb8d17ac0b48012
Ganning Xu <ganningxu@gmail.com>
2022-04-16 15:50:48 -0400

    add frontend

--- twocatz/index.js
+++ twocatz/index.js
@@ -4,31 +4,39 @@ const fetch = require("node-fetch");
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const firstCat = await getCatPic(context);
-  const secondCat = await getCatPic(context);
+  // www.example.com?name1=dave&name2=hello
+  // alternative: change this into an array
+  const name1 = req.query.name1;
+  const name2 = req.query.name2;
+  const name3 = req.query.name3;
+  const name4 = req.query.name4;
 
-  const namesArray = [];
-  namesArray.push(getNames());
-  namesArray.push(getNames());
+  context.log("Got the names...");
+
+  const cat1 = await getCatPic(name1);
+  const cat2 = await getCatPic(name2);
+  const cat3 = await getCatPic(name3);
+  const cat4 = await getCatPic(name4);
+
+  context.log("Got the images...");
 
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: {
-      cat1: firstCat,
-      cat2: secondCat,
-      names: namesArray,
+      cat1: cat1,
+      cat2: cat2,
+      cat3: cat3,
+      cat4: cat4,
     },
   };
 };
 
-async function getCatPic(context) {
-  const resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {
+async function getCatPic(name) {
+  const resp = await fetch("https://cataas.com/cat/says/" + name, {
     method: "GET",
   });
 
   const data = await resp.arrayBuffer();
-
-  context.log(data);
 
   const dataInBase64 = Buffer.from(data).toString("base64");
 
